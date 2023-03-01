29cc1ef62aac47ac7ab4697acd8d903b0d244e51
David <daivdpro@tuta.io>
2023-03-01 14:49:01 +0100

    tests

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,21 +1,24 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game4', function () {
+describe("Game4", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game4');
+    const Game = await ethers.getContractFactory("Game4");
     const game = await Game.deploy();
+    const signer0 = ethers.provider.getSigner(0);
+    const address0 = await signer0.getAddress();
 
-    return { game };
+    return { game, signer0, address0 };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, signer0, address0 } = await loadFixture(
+      deployContractAndSetVariables
+    );
 
-    // nested mappings are rough :}
-
-    await game.win();
+    await game.write(address0);
+    await game.win(address0);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
